1a715a5e7d7cadbf93daceaa1091a4511e729ce7
jasminevo <37910677+jasminevo@users.noreply.github.com>
2020-01-20 21:43:43 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  
+  
+  //Create function
+  function randomNum() {
+    return Math.floor((Math.random()*100));
+  }
+  
+  //Assign variable
+  let numberToGuess = randomNum();
+  console.log(numberToGuess);
+  
+  //Using if/else and value comparison
+  function checkGuess(guess) {
+    // Check guess against our random number
+    console.log(typeof guess)      //found out it returns as string
+    if (parseInt(guess) === numberToGuess) {  //== is just a bandaid, use parsing
+        alert("Correct Number")
+    } else {
+      alert("Guess again")
+    }
+    
+  }
+  
+// This code uses jQuery, a javascript library, to run.
+// You don't need to know how this works, 
+// just that it makes the submit button function.
+  $('.guessingForm').submit(function(event) {
+    event.preventDefault();
+    var guess = $('#js-user-guess').val();
+    $('#js-user-guess').val('');
+    checkGuess(guess);
+  });  
+// document.ready end    
+});
+
+
+
+// Possible additions
+// 1. Tracking number of guesses
+// 2. Hot or cold (more if statements)
+// 3. New game/reset button
+
